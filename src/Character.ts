8c896046f6247000559801b4bdedad8153b88807
thais viana <thaisviana081@outlook.com>
2022-11-21 16:55:20 -0300

    7 - Crie a classe Character

--- src/Character.ts
+++ src/Character.ts
@@ -2,9 +2,9 @@ import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter';
 import Race, { Elf } from './Races';
+import getRandomInt from './utils';
 
 class Character implements Fighter {
-  private _name: string;
   private _race: Race;
   private _archetype: Archetype;
   private _maxLifePoints: number;
@@ -17,10 +17,9 @@ class Character implements Fighter {
   constructor(
     name: string,
   ) {
-    this._name = name;
     this._dexterity = Character.randomNumber();
-    this._race = new Elf(this._name, this._dexterity);
-    this._archetype = new Mage(this._name);
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
     this._maxLifePoints = this._race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
     this._strength = Character.randomNumber();
@@ -31,8 +30,75 @@ class Character implements Fighter {
     };
   }
 
-  static randomNumber() {
-    return Math.floor(Math.random() * 10 + 1);
+  static randomNumber(): number {
+    return getRandomInt(1, 10);
+  }
+
+  // Os atributos da classe Character podem ser lidos mas não podem ser alterados 
+  
+  get race() {
+    return this._race;
+  }
+
+  get archetype() {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get energy(): Energy { //  Lembre-se que energy é um objeto, portanto se você retornar ele diretamente o javascript permite que as propriedades desse objetos sejam alteradas, mesmo energy sendo privado
+    return { ...this._energy };
+  }
+
+  // os métodos estendidos da interface Fighter
+  receiveDamage(attackPoints:number): number {
+    const damage = attackPoints - this._defense;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+    if (damage <= 0) {
+      this._lifePoints -= 1;
+    }
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this.lifePoints;
+  }
+
+  attack(enemy:Fighter) {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp() {
+    this._maxLifePoints += Character.randomNumber();
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    
+    this._strength += Character.randomNumber();
+    this._dexterity += Character.randomNumber();
+    this._defense += Character.randomNumber();
+    this._energy.amount = 10;
+    this._lifePoints = this._maxLifePoints;
+  }
+
+  special(enemy:Fighter) {
+    enemy.receiveDamage(this._lifePoints -= 20);
   }
 }
 

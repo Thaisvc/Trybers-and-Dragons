883cee532465f67479b7e554a7517d01a857fd61
thais viana <thaisviana081@outlook.com>
2022-11-21 17:35:39 -0300

    09 ERRO TEST CONSTRUCTOR

---
+++ src/Monster.ts
@@ -0,0 +1,38 @@
+import { SimpleFighter } from './Fighter/index';
+
+class Monster implements SimpleFighter {
+  constructor(private _lifePoints: number, private _strength: number) {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  // Os atributos da classe
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  // os métodos 
+  receiveDamage(attackPoints: number):number {
+    const damage = attackPoints - this._lifePoints;
+
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this.lifePoints;
+  }
+
+  attack(enemy:SimpleFighter): void {
+    this.receiveDamage(enemy.strength);
+  }
+}
+
+export default Monster;

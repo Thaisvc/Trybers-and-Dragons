955584de1b410353162ef6cc5eff14eca7b17fae
thais viana <thaisviana081@outlook.com>
2022-11-21 17:38:11 -0300

    ERRO CONSTRUCTOR RESOLVIDO

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,7 +1,10 @@
 import { SimpleFighter } from './Fighter/index';
 
 class Monster implements SimpleFighter {
-  constructor(private _lifePoints: number, private _strength: number) {
+  private _lifePoints: number;
+  private _strength: number;
+  
+  constructor() {
     this._lifePoints = 85;
     this._strength = 63;
   }
